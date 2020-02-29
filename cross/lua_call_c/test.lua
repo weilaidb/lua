654346ac78df6a8fabe43fb0896d08cae24ae7a0
CentOS-7-X86_64 <duo@localhost.localdomain>
2020-02-29 21:15:20 +0800

    move cross to lua call c

---
+++ cross/lua_call_c/test.lua
@@ -0,0 +1,12 @@
+print("========Lua脚本=======")
+--add为C导入Lua函数
+sum = addx(10,20)
+print("计算结果为：")
+print(sum)
+
+--sub为C导入Lua函数
+result = sub(10,20000)
+print("计算结果为：")
+print(result)
+
+print("========Lua脚本=======")
