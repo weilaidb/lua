e136b7a3f4ad0f04fce536fceba4a28bf5f6d893
CentOS-7-X86_64 <duo@localhost.localdomain>
2020-03-01 10:17:10 +0800

    add some lua express of base

--- cross/lua_call_c/test.lua
+++ cross/lua_call_c/test.lua
@@ -1,3 +1,5 @@
+#!/usr/local/bin/lua
+
 print("========Lua脚本=======")
 --add为C导入Lua函数
 sum = addx(10,20)
@@ -10,3 +12,517 @@ print("计算结果为：")
 print(result)
 
 print("========Lua脚本=======")
+print("Hello World!")
+print("www.runoob.com")
+print(b)
+print(type("Hello world"))      --> string
+print(type(10.4*3))             --> number
+print(type(print))              --> function
+print(type(type))               --> function
+print(type(true))               --> boolean
+print(type(nil))                --> nil
+print(type(type(X)))            --> string
+
+
+print ("=====================")
+tab1 = { key1 = "val1", key2 = "val2", "val3" }
+for k, v in pairs(tab1) do
+    print(k .. " - " .. v)
+end
+ 
+tab1.key1 = nil
+for k, v in pairs(tab1) do
+    print(k .. " - " .. v)
+end
+
+
+print ("=====================")
+print(type(true))
+print(type(false))
+print(type(nil))
+ 
+if false or nil then
+    print("至少有一个是 true")
+else
+    print("false 和 nil 都为 false")
+end
+
+if 0 then
+    print("数字 0 是 true")
+else
+    print("数字 0 为 false")
+end
+
+print ("=====================")
+
+
+print(type(2))
+print(type(2.2))
+print(type(0.2))
+print(type(2e+1))
+print(type(0.2e-1))
+print(type(7.8263692594256e-06))
+
+
+print ("=====================")
+
+html = [[
+<html>
+<head></head>
+<body>
+    <a href="http://www.runoob.com/">菜鸟教程</a>
+</body>
+</html>
+]]
+print(html)
+
+
+print ("=====================")
+
+print("2" + 6)
+
+print("2" + "6")
+
+print("2 + 6")
+
+print("-2e2" * "6")
+
+
+--print("error" + 1)
+
+print ("=====================")
+
+print("a" .. 'b')
+print(157 .. 428)
+
+
+print ("=====================")
+len = "www.runoob.com"
+print(#len)
+print(#"www.runoob.com")
+
+
+print ("=====================")
+a = {}
+a["key"] = "value"
+a["xin"] = "jing"
+a["emituofo"] = "wuliangjue"
+key = 10
+a[key] = 22
+a[key] = a[key] + 11
+for k, v in pairs(a) do
+    print(k .. " : " .. v)
+end
+
+
+print ("=====================")
+
+local tbl = {"apple", "pear", "orange", "grape"}
+for key, val in pairs(tbl) do
+    print("Key", key, "val", val)
+end
+
+
+print ("=====================")
+
+a3 = {}
+for i = 1, 10 do
+    a3[i] = i
+end
+a3["key"] = "val"
+print(a3["key"])
+print(a3["none"])
+
+for key,val in pairs(a3) do
+	print(key, val)
+end
+
+
+print ("=========function============")
+
+function factorial1(n)
+    if n == 0 then
+        return 1
+    else
+        return n * factorial1(n - 1)
+    end
+end
+print(factorial1(5))
+factorial2 = factorial1
+print(factorial2(5))
+
+
+print ("=========function 2============")
+
+function testFun(tab,fun)
+        for k ,v in pairs(tab) do
+                print(fun(k,v));
+        end
+end
+
+
+tab={key1="val1",key2="val2"};
+testFun(tab,
+function(key,val)--匿名函数
+        return key.."=="..val;
+end
+);
+
+
+
+
+print ("=========variable 1============")
+
+a = 5               -- 全局变量
+local b = 5         -- 局部变量
+
+function joke()
+    c = 5           -- 全局变量
+    local d = 6     -- 局部变量
+end
+
+joke()
+print(c,d)          --> 5 nil
+
+do
+    local a = 6     -- 局部变量
+    b = 6           -- 对局部变量重新赋值
+    print(a,b);     --> 6 6
+end
+
+print(a,b)      --> 5 6
+
+
+print ("=========variable 2============")
+
+a, b, c = 0, 1
+print(a,b,c)             --> 0   1   nil
+ 
+a, b = a+1, b+1, b+2     -- value of b+2 is ignored
+print(a,b)               --> 1   2
+ 
+a, b, c = 0
+print(a,b,c)             --> 0   nil   nil
+
+
+
+print ("=========variable 3============")
+
+site = {}
+site["key"] = "www.runoob.com"
+print(site["key"])
+print(site.key)
+
+print ("=========while loop============")
+----
+----while( true )
+----do
+----   print("循环将永远执行下去")
+----end
+
+print ("=========like continue============")
+for i = 10, 1, -1 do
+  repeat
+    if i == 5 then
+      print("continue code here")
+      break
+    end
+    print(i, "loop code here")
+  until true
+end
+
+print ("=========if============")
+--[ 0 为 true ]
+if(0)
+then
+    print("0 为 true")
+end
+
+
+print ("=========if else============")
+
+--[ 定义变量 --]
+a = 100;
+--[ 检查条件 --]
+if( a < 20 )
+then
+   --[ if 条件为 true 时执行该语句块 --]
+   print("a 小于 20" )
+else
+   --[ if 条件为 false 时执行该语句块 --]
+   print("a 大于 20" )
+end
+print("a 的值为 :", a)
+
+
+print ("=========if...elseif...else 语句============")
+
+--[ 定义变量 --]
+a = 100
+
+--[ 检查布尔条件 --]
+if( a == 10 )
+then
+   --[ 如果条件为 true 打印以下信息 --]
+   print("a 的值为 10" )
+elseif( a == 20 )
+then  
+   --[ if else if 条件为 true 时打印以下信息 --]
+   print("a 的值为 20" )
+elseif( a == 30 )
+then
+   --[ if else if condition 条件为 true 时打印以下信息 --]
+   print("a 的值为 30" )
+else
+   --[ 以上条件语句没有一个为 true 时打印以下信息 --]
+   print("没有匹配 a 的值" )
+end
+print("a 的真实值为: ", a )
+
+
+print ("=========function============")
+--[[ 函数返回两个值的最大值 --]]
+function max(num1, num2)
+
+   if (num1 > num2) then
+      result = num1;
+   else
+      result = num2;
+   end
+
+   return result;
+end
+-- 调用函数
+print("两值比较最大值为 ",max(10,4))
+print("两值比较最大值为 ",max(5,6))
+
+
+print ("=========function 2,return one value============")
+myprint = function(param)
+   print("这是打印函数 -   ##",param,"##")
+end
+
+function add(num1,num2,functionPrint)
+   result = num1 + num2
+   -- 调用传递的函数参数
+   functionPrint(result)
+end
+myprint(10)
+-- myprint 函数作为参数传递
+add(2,5,myprint)
+
+print ("=========function 2,return multi value============")
+function maximum (a)
+    local mi = 1             -- 最大值索引
+    local m = a[mi]          -- 最大值
+    for i,val in ipairs(a) do
+       if val > m then
+           mi = i
+           m = val
+       end
+    end
+    return m, mi
+end
+
+print(maximum({8,10,23,12,5}))
+
+
+print ("=========function ,vargs 可变参数============")
+function average(...)
+   result = 0
+   local arg={...}    --> arg 为一个表，局部变量
+   for i,v in ipairs(arg) do
+      result = result + v
+   end
+   print("总共传入 " .. #arg .. " 个数")
+   return result/#arg
+end
+
+print("平均值为",average(10,5,3,4,5,6))
+
+print ("=========function ,通过 select(\"#\",...) 来获取可变参数的数量============")
+function average(...)
+   result = 0
+   local arg={...}
+   for i,v in ipairs(arg) do
+      result = result + v
+   end
+   print("总共传入 " .. select("#",...) .. " 个数")
+   return result/select("#",...)
+end
+
+print("平均值为",average(10,5,3,4,5,6))
+
+
+print ("=========function 3============")
+function fwrite(fmt, ...)  ---> 固定的参数fmt
+    return io.write(string.format(fmt, ...))    
+end
+
+fwrite("runoob\n")       --->fmt = "runoob", 没有变长参数。  
+fwrite("%d%d\n", 1, 2)   --->fmt = "%d%d", 变长参数为 1 和 2
+
+
+
+print ("=========function 4============")
+do  
+    function foo(...)  
+        for i = 1, select('#', ...) do  -->获取参数总数
+            local arg = select(i, ...); -->读取参数
+            print("arg", arg);  
+        end  
+    end  
+ 
+    foo(1, 2, 3, 4);  
+end
+
+print ("=========   算术运算符  ============")
+a = 21
+b = 10
+c = a + b
+print("Line 1 - c 的值为 ", c )
+c = a - b
+print("Line 2 - c 的值为 ", c )
+c = a * b
+print("Line 3 - c 的值为 ", c )
+c = a / b
+print("Line 4 - c 的值为 ", c )
+c = a % b
+print("Line 5 - c 的值为 ", c )
+c = a^2
+print("Line 6 - c 的值为 ", c )
+c = -a
+print("Line 7 - c 的值为 ", c )
+
+print ("=========   关系运算符  ============")
+a = 21
+b = 10
+
+if( a == b )
+then
+   print("Line 1 - a 等于 b" )
+else
+   print("Line 1 - a 不等于 b" )
+end
+
+if( a ~= b )
+then
+   print("Line 2 - a 不等于 b" )
+else
+   print("Line 2 - a 等于 b" )
+end
+
+if ( a < b )
+then
+   print("Line 3 - a 小于 b" )
+else
+   print("Line 3 - a 大于等于 b" )
+end
+
+if ( a > b )
+then
+   print("Line 4 - a 大于 b" )
+else
+   print("Line 5 - a 小于等于 b" )
+end
+
+-- 修改 a 和 b 的值
+a = 5
+b = 20
+if ( a <= b )
+then
+   print("Line 5 - a 小于等于  b" )
+end
+
+if ( b >= a )
+then
+   print("Line 6 - b 大于等于 a" )
+end
+
+
+print ("=========   逻辑运算符  ============")
+
+a = true
+b = true
+
+if ( a and b )
+then
+   print("a and b - 条件为 true" )
+end
+
+if ( a or b )
+then
+   print("a or b - 条件为 true" )
+end
+
+print("---------分割线---------" )
+
+-- 修改 a 和 b 的值
+a = false
+b = true
+
+if ( a and b )
+then
+   print("a and b - 条件为 true" )
+else
+   print("a and b - 条件为 false" )
+end
+
+if ( not( a and b) )
+then
+   print("not( a and b) - 条件为 true" )
+else
+   print("not( a and b) - 条件为 false" )
+end
+
+
+print ("=========   其他运算符  ============")
+
+a = "Hello "
+b = "World"
+
+print("连接字符串 a 和 b ", a..b )
+
+print("b 字符串长度 ",#b )
+
+print("字符串 Test 长度 ",#"Test" )
+
+print("菜鸟教程网址长度 ",#"www.runoob.com" )
+
+
+
+print ("=========   运算符优先级  ============")
+
+a = 20
+b = 10
+c = 15
+d = 5
+
+e = (a + b) * c / d;-- ( 30 * 15 ) / 5
+print("(a + b) * c / d 运算值为  :",e )
+
+e = ((a + b) * c) / d; -- (30 * 15 ) / 5
+print("((a + b) * c) / d 运算值为 :",e )
+
+e = (a + b) * (c / d);-- (30) * (15/5)
+print("(a + b) * (c / d) 运算值为 :",e )
+
+e = a + (b * c) / d;  -- 20 + (150/5)
+print("a + (b * c) / d 运算值为   :",e )
+
+
+print ("=========   Lua 字符串  ============")
+string1 = "Lua"
+print("\"字符串 1 是\"",string1)
+string2 = 'runoob.com'
+print("字符串 2 是",string2)
+
+string3 = [["Lua 教程"]]
+print("字符串 3 是",string3)
+print("字符串 3 是",string.upper(string3))
+print("字符串 3 是",string.lower(string3))
+print("字符串 3 是",string.gsub(string3,"u","z",3))
+
+
+
+
